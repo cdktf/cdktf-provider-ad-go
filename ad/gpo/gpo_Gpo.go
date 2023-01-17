4f6d2a29cd03d8def6a1395e20e39e452ea03bdc
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2023-01-17 14:59:08 +0000

    chore(release): v3.0.0

--- ad/gpo/gpo_Gpo.go
+++ ad/gpo/gpo_Gpo.go
@@ -2,10 +2,10 @@ package gpo
 
 import (
 	_jsii_ "github.com/aws/jsii-runtime-go/runtime"
-	_init_ "github.com/cdktf/cdktf-provider-ad-go/ad/v2/jsii"
+	_init_ "github.com/cdktf/cdktf-provider-ad-go/ad/v3/jsii"
 
 	"github.com/aws/constructs-go/constructs/v10"
-	"github.com/cdktf/cdktf-provider-ad-go/ad/v2/gpo/internal"
+	"github.com/cdktf/cdktf-provider-ad-go/ad/v3/gpo/internal"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 
--- ad/gposecurity/gposecurity_GpoSecurityRestrictedGroupsList.go
+++ ad/gposecurity/gposecurity_GpoSecurityRestrictedGroupsList.go
@@ -2,9 +2,9 @@ package gposecurity
 
 import (
 	_jsii_ "github.com/aws/jsii-runtime-go/runtime"
-	_init_ "github.com/cdktf/cdktf-provider-ad-go/ad/v2/jsii"
+	_init_ "github.com/cdktf/cdktf-provider-ad-go/ad/v3/jsii"
 
-	"github.com/cdktf/cdktf-provider-ad-go/ad/v2/gposecurity/internal"
+	"github.com/cdktf/cdktf-provider-ad-go/ad/v3/gposecurity/internal"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 
